708f4080ed6789e4adae89f9de7c034d8f9af333
RC-83 <rcholewinski83@wp.pl>
2022-08-07 15:43:27 +0200

    lesson-7.2 git commit -m lesson-7.2 git status
    cd..

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const postCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -17,6 +18,10 @@ module.exports = {
     },
    
     devServer: {
+        onBeforeSetupMiddleware: function(app, server){
+            watchFiles: ['./app/**/*.html']
+        },
+        
         static: path.join(__dirname, 'app'),
         hot:true,
         port: 3000, 
